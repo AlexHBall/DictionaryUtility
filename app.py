a78287da15247f28d5de5fe4000ded4cd82afb9d
AlexHBall <timmygaines22@gmail.com>
2020-08-09 16:50:22 +0200

    split endpoints

--- app.py
+++ app.py
@@ -7,10 +7,14 @@ app.config["DEBUG"] = True
 app.config['JSON_SORT_KEYS'] = False
 
 @app.route('/<word>')
-def profile(word):
+def word(word):
     entry = Dictonary_Entry(word)
     return jsonify({
       'word' : word,
       'defitions' : entry.defitions
     }), 200
 
+@app.route('/<word>/pronounciation')
+def summary(word):
+    entry = Dictonary_Entry(word)
+    return entry.get_pronounciation(), 200
