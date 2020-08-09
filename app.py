9a6bff3530848a71183a3c21111c96c6be63fbff
AlexHBall <timmygaines22@gmail.com>
2020-08-09 16:08:08 +0200

    remove tutorial code

--- app.py
+++ app.py
@@ -6,14 +6,6 @@ app = Flask(__name__)
 app.config["DEBUG"] = True
 app.config['JSON_SORT_KEYS'] = False
 
-
-@app.route('/fortune', methods=['GET'])
-def fortune():
-    return jsonify({
-        'data': 'How many of you believe in psycho-kinesis? Raise my hand.',
-        'status': 'awesome'
-    }), 200
-
 @app.route('/<word>')
 def profile(word):
     entry = Dictonary_Entry(word)
