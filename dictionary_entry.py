b90c8e33891e4a255566d542a354ef8711eb6717
AlexHBall <timmygaines22@gmail.com>
2020-08-09 15:20:08 +0200

    handle word not found

--- dictionary_entry.py
+++ dictionary_entry.py
@@ -1,16 +1,31 @@
 from lxml import etree
 import os
+import unicodedata
 
 class Dictonary_Entry:
     def __init__(self, word):
         root = self.load_dict(word)
         element = self.search_word(word, root)
+        if not element:
+            raise Exception("Word not found")
         self.defitions = self.get_body_variants(element)   
 
     def load_dict(self,word):
-    # TODO: What happens when the word starts with an accent e.g. tre
+        def strip_accents(text):
+            try:
+                text = unicode(text, 'utf-8')
+            except NameError: # unicode is a default on python 3 
+                pass
+
+            text = unicodedata.normalize('NFD', text)\
+                .encode('ascii', 'ignore')\
+                .decode("utf-8")
+
+            return str(text)
+
+        first_character = strip_accents(word[0].lower())
         script_dir = os.path.dirname(__file__)
-        relative_path = "data/{}.xml".format(word[0].lower())
+        relative_path = "data/{}.xml".format(first_character)
         abs_file_path = os.path.join(script_dir, relative_path)
 
         with open(abs_file_path) as f:
@@ -21,10 +36,10 @@ class Dictonary_Entry:
 
     def search_word(self,word, root):
         try:
-            return root.xpath("//entree[@terme='{}']".format(word.upper()))[0]
+            element = root.xpath("//entree[@terme='{}']".format(word.upper()))[0]
         except IndexError:
-            #TODO: How to handle erros
-            return ""
+            return None
+        return element
 
 
     def get_body_variants(self,element):
@@ -33,6 +48,7 @@ class Dictonary_Entry:
         for variant in variants:
             text = variant.text
             if text is not None:
+                #TODO: Handle unicode properly
                 text = text.replace("\n","")
                 text = text.replace("\xa0"," ")
                 texts.append(text.strip())
